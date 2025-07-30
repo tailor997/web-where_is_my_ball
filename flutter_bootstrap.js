1b4fca07a724ee1a00326ac67e17af5deca53092
tech_surfer <zhangzhe@panchip.com>
2025-07-31 00:18:41 +0800

    build:base on 7c92da85c3e5bf4d8b45c8e6e55a1cc851deee44

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3370234983"
+    serviceWorkerVersion: "2888569963"
   }
 });
